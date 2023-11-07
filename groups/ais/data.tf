e2ae452ae0f857ced16ba715f5da29ad4bcdcf65
Marc Ransome <mransome@companieshouse.gov.uk>
2023-11-07 12:12:05 +0000

    Refactor codebase for Terraform 1.3 migration

---
+++ groups/ais/data.tf
@@ -0,0 +1,67 @@
+data "aws_vpc" "heritage" {
+  filter {
+    name   = "tag:Name"
+    values = ["vpc-heritage-${var.environment}"]
+  }
+}
+
+data "aws_subnets" "application" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.heritage.id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = [var.application_subnet_pattern]
+  }
+}
+
+data "aws_subnet" "application" {
+  count = length(data.aws_subnets.application.ids)
+  id    = tolist(data.aws_subnets.application.ids)[count.index]
+}
+
+data "aws_ami" "ais_tuxedo" {
+  owners      = [var.ami_owner_id]
+  most_recent = true
+  name_regex  = "^${var.service_subtype}-${var.service}-ami-\\d.\\d.\\d"
+
+  filter {
+    name   = "name"
+    values = ["${var.service_subtype}-${var.service}-ami-${var.ami_version_pattern}"]
+  }
+}
+
+data "cloudinit_config" "config" {
+  count = var.instance_count
+
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/cloud-config"
+    content      = templatefile("${path.module}/cloud-init/templates/system-config.yml.tpl", {})
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content = templatefile("${path.module}/cloud-init/templates/bootstrap-commands.yml.tpl", {
+      instance_hostname       = "${var.service_subtype}-${var.service}-${var.environment}-${count.index + 1}"
+      lvm_block_devices       = var.lvm_block_devices
+      root_volume_device_node = data.aws_ami.ais_tuxedo.root_device_name
+    })
+  }
+}
+
+data "vault_generic_secret" "kms_keys" {
+  path = "aws-accounts/${var.aws_account}/kms"
+}
+
+data "vault_generic_secret" "security_s3_buckets" {
+  path = "aws-accounts/security/s3"
+}
+
+data "vault_generic_secret" "security_kms_keys" {
+  path = "aws-accounts/security/kms"
+}
